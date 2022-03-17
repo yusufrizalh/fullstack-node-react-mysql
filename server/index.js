08ce2da09ca7ebf5daacec2e25b7c969f5d2bdd7
Yusuf Rizal <yusufrizalh@gmail.com>
2022-03-17 16:17:54 +0700

    update dotenv

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
+require("dotenv").config();
 
 const db = require("./models");
 const postRouter = require("./routes/Posts");
@@ -15,8 +16,13 @@ app.use("/comments", commentsRouter);
 app.use("/auth", usersRouter);
 app.use("/likes", likesRouter);
 
-db.sequelize.sync().then(() => {
-  app.listen(8001, () => {
-    console.log("Server running at http://127.0.0.1:8001");
+db.sequelize
+  .sync()
+  .then(() => {
+    app.listen(process.env.PORT || 8001, () => {
+      console.log("Server running at port: 8001");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
   });
-});
